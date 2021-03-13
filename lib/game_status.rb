91241500a6e3524d23f26ac89299cbca0269a5cd
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-03-13 18:18:03 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,15 +6,15 @@ end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
-def won?
+def won?(board)
   
 end
 
-def full?
+def full?(board)
   
 end
 
-def draw?
+def draw?(board)
   
 end
 
