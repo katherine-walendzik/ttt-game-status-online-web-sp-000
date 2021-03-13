6c72c8038ec27e3bcdabd925ef2bc89cbe7008ec
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-03-13 19:12:30 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,8 +17,9 @@ def won?(board)
   position_3 = board[win_index_3]
   
   if position_1 == position_2 && position_2 == position_3 && position_taken(board, win_index_1)
-  
+  end
 end
+
 
 def full?(board)
   
